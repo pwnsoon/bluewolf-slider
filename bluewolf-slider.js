c7418289d71cd3674934cb7a5f5d5baeb6d60824
Sławomir Jopek <sjopek86@gmail.com>
2017-05-03 23:40:07 +0200

    init

---
+++ bluewolf-slider.js
@@ -0,0 +1,21 @@
+(function() {
+    'use strict';
+
+    var defaults = {
+        // default options
+    };
+
+    var BwSlider = function(container, options) {
+        if (!(this instanceof BwSlider)) return new BwSlider(container, options);
+
+        // @TODO check if container is typeof HTMLElement
+        // @TODO set settings (merge options & defaults)
+    };
+
+    // @TODO add methods
+    BwSlider.prototype.buildSlider = function() {};
+
+    BwSlider.prototype.buildControls = function() {};
+
+    BwSlider.prototype.moveTo = function() {};
+}());
